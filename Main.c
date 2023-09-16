90154f8caee1b683e29419f56aef217ec2004c09
ali363 <56528027+ali363@users.noreply.github.com>
2023-09-16 07:49:42 +0530

    Initial commit

---
+++ Main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    printf("Ali Hi\n");
+    printf("HI Ali");
+    return 0;
+}
---
+++ add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a2;
+    
+    printf("Enter Number a2\n");
+    scanf("%d\n", &a2);
+    int b2;
+    printf("Enter Number b2\n");
+    scanf("%d\n", &b2);
+    printf("The Answer Is  %d\n", a2-b2);
+    return 0;
+}
